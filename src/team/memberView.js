a53398293623c4c254e76d62957d6c5669a20f41
Helen Thi <ht5658@gmail.com>
2016-12-27 10:33:44 -0500

    remove unnecessary parentheses for returning react elements

--- src/team/memberView.js
+++ src/team/memberView.js
@@ -4,18 +4,16 @@ import teamMembersObj from './teamMembers'
 // A React element that displays a team member
 
 export default (member, styles)  => 
-  (
-    <div className={styles}>
-      <div className="work-container">
-       <a href={"/team/" + member}>
-        <div className="work-img">
-          <img src={teamMembersObj[member].memberImg} alt=""/>
-        </div>
-        </a>
-        <div className="work-description">
-          <h3><a href={"/team/" + member}>{teamMembersObj[member].memberFullName}</a></h3>
-          <span><a href={"/team/" + member}>{teamMembersObj[member].memberRole}</a></span>
-        </div>
+  <div className={styles}>
+    <div className="work-container">
+     <a href={"/team/" + member}>
+      <div className="work-img">
+        <img src={teamMembersObj[member].memberImg} alt=""/>
+      </div>
+      </a>
+      <div className="work-description">
+        <h3><a href={"/team/" + member}>{teamMembersObj[member].memberFullName}</a></h3>
+        <span><a href={"/team/" + member}>{teamMembersObj[member].memberRole}</a></span>
       </div>
     </div>
-  )
+  </div>
--- src/team/teamView.js
+++ src/team/teamView.js
@@ -4,8 +4,6 @@ import teamMemberView from './memberView'
 // A React element that displays all team members
 
 export default (teamMembersObj, id, styles)  => 
-  (
-    <div id={id}>
+  <div id={id}>
     {Object.keys(teamMembersObj).map(member => (teamMemberView(member, styles)))}
-    </div>
-  )
+  </div>
