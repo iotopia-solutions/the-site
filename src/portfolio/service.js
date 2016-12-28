2d460c39c0d29bad6028f45e7720c61db6c69c73
Helen Thi <ht5658@gmail.com>
2016-12-28 14:36:15 -0500

    add route to each project page; set value for each project property

--- src/portfolio/service.js
+++ src/portfolio/service.js
@@ -9,6 +9,8 @@ import {portfolioObj} from './portfolio'
 // Load these early and sync, just like module dependencies.
 const requireTemplate = requireText(__dirname)
 const portfolioPageHtml = requireTemplate('./portfolioPage.html')
+const projectPageHtml = requireTemplate('./projectPage.html')
+
 
 // Handles a GET request
 export const portfolio
@@ -17,6 +19,24 @@ export const portfolio
     return (req, res) =>
       res.send(renderPortfolio({projects: transformProjects(portfolioObj)}))
   }
+
+export const project
+  = () => {
+    const renderPortfolio = compile(projectPageHtml)
+    return (req, res) =>
+      res.send(renderPortfolio(transformToViewData(req.params.id)))
+  }
+
+  const transformToViewData
+  = (id) => ({
+    clientName: portfolioObj[id].clientName,
+    category: portfolioObj[id].category,
+    projectTitle: portfolioObj[id].projectTitle,
+    technologies: portfolioObj[id].technologies,
+    projectLink: portfolioObj[id].projectLink,
+    projectImages: portfolioObj[id].projectImages,
+    description: portfolioObj[id].description,
+  })
 
   const transformProjects
     = (portfolioObj) => {
